4ea3d6ceaa382b86759f88316bb90222469a419c
Luciano Alves <ti4luciano@gmail.com>
2021-08-01 23:27:11 -0300

    Iniciando o script para pegar o token e setar na área de trasferência

---
+++ pegaToken.sh
@@ -0,0 +1,10 @@
+#######################################################################################
+#######################################################################################
+#-: Autor: Luciano Alves
+#-: Data: 01/08/2021
+#-: Finalidade: Buscar o token autorizador no servidor e setar na área de transferência.
+
+#!/bin/bash
+
+#Checar se tem os programas necessários para o correto funcionamento
+
